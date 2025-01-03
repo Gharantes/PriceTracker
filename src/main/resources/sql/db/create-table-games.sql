da77abeea7d04d366e6bf2f13217832cdd92faf1
Guilherme Arantes <work.gharantes@gmail.com>
2025-01-03 19:51:35 -0300

    Invalid Create Table Fixed

--- src/main/resources/sql/db/create-table-games.sql
+++ src/main/resources/sql/db/create-table-games.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS games (
-    id BIGINT PRIMARY KEY AUTOINCREMENT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT not null,
     steam_id TEXT,
     target_price numeric (6, 2), -- MAX: 9999,99

c2b77e1a397eb8d2027f5adf9e55fa9135dedcca
Guilherme Arantes <work.gharantes@gmail.com>
2025-01-03 17:09:40 -0300

    Default App Size
    Sets the default window width & height for the app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,24 @@
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import compose.App
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
+private const val WIDTH = 500
+private const val HEIGHT = 720
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = rememberWindowState(
+            size = DpSize(
+                width = WIDTH.dp,
+                height = HEIGHT.dp
+            )
+        )
+    ) {
         App()
     }
 }
---
+++ src/main/kotlin/compose/App.kt
@@ -0,0 +1,9 @@
+package compose
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun App() {
+    Text(text = "Hello World")
+}

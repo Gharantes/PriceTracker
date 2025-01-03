ab3d2d642e89303e0d297248122e84cf1b4a142a
Guilherme Arantes <work.gharantes@gmail.com>
2025-01-03 17:00:55 -0300

    Initial Kotlin Compose Files

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm") version "2.0.21"
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+group = "com.price-tracker"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "PriceTracker"
+            packageVersion = "1.0.0"
+        }
+    }
+}

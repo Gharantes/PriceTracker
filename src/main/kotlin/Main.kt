942e3dca9f2f33e0c494b11c6e8720bfd136db1c
Guilherme Arantes <work.gharantes@gmail.com>
2025-01-03 18:10:57 -0300

    Database Utils. Initialize currently disabled.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,12 +4,14 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import compose.App
+import core.utils.DatabaseUtils
 
 
 private const val WIDTH = 500
 private const val HEIGHT = 720
 
 fun main() = application {
+    DatabaseUtils().initialize()
     Window(
         onCloseRequest = ::exitApplication,
         state = rememberWindowState(
---
+++ src/main/kotlin/core/utils/DatabaseUtils.kt
@@ -0,0 +1,67 @@
+package core.utils
+
+import java.io.File
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.ResultSet
+import java.sql.Statement
+
+private const val DATABASE_BASE_PATH = "game-price-checker.db"
+
+class DatabaseUtils {
+    private val resourceUtils = ResourceUtils()
+
+    fun initialize() {
+//        require(dbExists()) { "Database could not be found." }
+//        createTable()
+    }
+    private fun dbExists(): Boolean {
+        val directory = System.getProperty("user.dir")
+        val dbPath = File(directory, DATABASE_BASE_PATH)
+        val dbParent = dbPath.parentFile
+        if (!dbParent.exists()) {
+            dbParent.mkdirs()
+        }
+        return dbPath.exists()
+    }
+    private fun createTable() {
+        val path = "/sql/db/create_table_games.sql"
+        execute(path, ::executeUpdate)
+    }
+
+
+    fun <T> execute (
+        path: String,
+        executorType: (Statement, String) -> T
+    ): T? {
+        return try {
+            val connection = getDriveConnection()
+            val statement = connection.createStatement()
+            val result = executorType(
+                statement,
+                resourceUtils.getFileContentFromResources(path)
+            )
+            statement.close()
+            connection.close()
+            result
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+    fun executeUpdate(
+        statement: Statement,
+        sql: String
+    ) {
+        statement.executeUpdate(sql)
+    }
+    fun executeQuery(
+        statement: Statement,
+        sql: String
+    ): ResultSet {
+        return statement.executeQuery(sql)
+    }
+    private fun getDriveConnection(): Connection {
+        return DriverManager.getConnection("jdbc:sqlite:$DATABASE_BASE_PATH")
+    }
+}
